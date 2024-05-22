66df713b1170203c76751bae46f4928ab6652004
shuvo90901 <shuvo90901@gmail.com>
2024-05-22 13:25:50 +0600

    server running

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+// import { StudentRoutes } from './app/modules/student/student.route'
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+// app.use('/api/v1/students', StudentRoutes)
+
+const getAController = (req: Request, res: Response) => {
+  const a = 'Hello'
+  res.send(a)
+}
+
+app.get('/', getAController)
+
+export default app
